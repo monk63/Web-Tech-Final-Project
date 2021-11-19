c78764f34845962be1af470713088bf3910bf8fa
Mike Ofori <61565391+monk63@users.noreply.github.com>
2021-11-19 22:50:18 +0000

    vedvevc

--- js/script.js
+++ js/script.js
@@ -1,11 +1,38 @@
 
 
-//Icons on home page
+
 
 
 
 // Vechicle Slider on used car page
 var swiper = new Swiper(".vehicles-slider", {
+    grabCursor: true,
+    centeredSlides: true,  
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+// Review on about page
+  var swiper = new Swiper(".review-slider", {
     grabCursor: true,
     centeredSlides: true,  
     spaceBetween: 20,
